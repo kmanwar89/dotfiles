ce6d59f6555826fa570ecf7a0ef24bfd72acc915
Kadar Anwar <anwarkm@dukes.jmu.edu>
2019-08-12 11:46:09 -0400

    Fixed disable touchpad while typing configuration in i3 config file

--- scripts/touchpad/touchon.sh
+++ scripts/touchpad/touchon.sh
@@ -12,7 +12,7 @@ xinput disable "Synaptics s3203_ver5"
 #notify-send "Touchpad" "Touchpad Enabled" -i /path/to/enabled.jpg -t 1000
 
 # Keyboard delay of 1 second after finish typing
-syndaemon -i 1 -d
+# syndaemon -i 1 -d
 
 # Natural Scrolling
 #synclient VertScrollDelta=-30
