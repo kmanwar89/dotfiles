06a61fc8ad579326ccab95c0f829c61de40d27d9
Kadar Anwar <anwarkm@dukes.jmu.edu>
2019-08-12 11:47:01 -0400

    Removed natural scrolling & keyboard delay from scripts

--- scripts/touchpad/touchon.sh
+++ scripts/touchpad/touchon.sh
@@ -10,10 +10,3 @@ xinput disable "Synaptics s3203_ver5"
 # Show a notification
 # Doesn't currently work 6/21/2016
 #notify-send "Touchpad" "Touchpad Enabled" -i /path/to/enabled.jpg -t 1000
-
-# Keyboard delay of 1 second after finish typing
-# syndaemon -i 1 -d
-
-# Natural Scrolling
-#synclient VertScrollDelta=-30
-#synclient HorizScrollDelta=-30
