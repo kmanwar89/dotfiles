d36b814d8c8ff224b5e921941d84c5afb1a84787
Kadar Anwar <kadaranwar07@gmail.com>
2020-09-30 19:07:22 -0400

    Added non-root commands for yay and removed unnecessary sudo

--- scripts/5post.sh
+++ scripts/5post.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 # Purpose: Configure system after initial reboot
 
-# Install yay
-#git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# Switch to non-root user for yay commands
+su kadar
+cd ~
+yay -Sy --noconfirm lxdm-themes
+exit
 
 # Install & configure LXDM themes
-#yay -Sy --noconfirm lxdm-themes
-#sed -i 's/theme=Industrial/theme=Arch-Dark/g' /etc/lxdm/lxdm.conf
-#sed -i 's/# bg=\/usr\/share\/backgrounds\/default.png/bg=\/usr\/share\/lxdm\/themes\/Arch-Dark\/archdark.jpg/g' /etc/lxdm/lxdm.conf
+sed -i 's/theme=Industrial/theme=Arch-Dark/g' /etc/lxdm/lxdm.conf
+sed -i 's/# bg=\/usr\/share\/backgrounds\/default.png/bg=\/usr\/share\/lxdm\/themes\/Arch-Dark\/archdark.jpg/g' /etc/lxdm/lxdm.conf
 
 # Install & configure rEFInd themes
-#sudo mkdir /efi/EFI/refind/themes
-#git clone https://github.com/mireq/rEFInd-Catalina.git && cd rEFInd-Catalina/refind/ && sudo cp -r Catalina /efi/EFI/refind/themes/
-#echo $'\n\n'# rEFInd Catalina Theme$'\n'#Source: https://github.com/mireq/rEFInd-Catalina$'\n'include themes/Catalina/theme.conf >> /efi/EFI/refind/refind.conf
-#sed -i 's/#resolution 3/resolution 1920 1080/g' /efi/EFI/refind/refind.conf
+mkdir /efi/EFI/refind/themes
+git clone https://github.com/mireq/rEFInd-Catalina.git && cd rEFInd-Catalina/refind/ && sudo cp -r Catalina /efi/EFI/refind/themes/
+echo $'\n\n'# rEFInd Catalina Theme$'\n'#Source: https://github.com/mireq/rEFInd-Catalina$'\n'include themes/Catalina/theme.conf >> /efi/EFI/refind/refind.conf
+sed -i 's/#resolution 3/resolution 1920 1080/g' /efi/EFI/refind/refind.conf
 
 # Install audio
 pacman -Sy --noconfirm pulseaudio pavucontrol pulseaudio-alsa alsa-utils
