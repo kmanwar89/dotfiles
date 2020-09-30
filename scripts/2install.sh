0b90eb6066396d7b9514de82c1e35906867ca738
Kadar Anwar <kadaranwar07@gmail.com>
2020-09-30 11:57:41 -0400

    Update 2install.sh

--- scripts/2install.sh
+++ scripts/2install.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
+
 # Purpose of this script is to automate the remaining ArchLinux install commands prior to chrooting into the installed environment
 timedatectl set-ntp true
 pacman -Sy --noconfirm reflector
 reflector --country US --country Canada --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 sleep 5
+
 # Installation of base system
 pacstrap /mnt base base-devel linux linux-firmware
 sleep 2
