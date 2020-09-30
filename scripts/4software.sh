1dd8c4308f900132b553cd46f72029f6bce4acb8
Kadar Anwar <kadaranwar07@gmail.com>
2020-09-30 19:19:02 -0400

    More programs
    Added additional system software

--- scripts/4software.sh
+++ scripts/4software.sh
@@ -9,7 +9,7 @@ exit
 echo "Installing base software (audio, bluetooth, others)"
 echo " "
 sleep 1
-pacman -Sy --noconfirm lxdm xorg-xinit mesa xterm xorg-twm xorg-apps xorg-xclock i3-wm i3status i3lock dmenu perl-anyevent-i3 perl-json-xs synaptics net-tools light pulseaudio pavucontrol pulseaudio-alsa alsa-utils openssh vi ttf-font-awesome ttf-dejavu dialog wpa_supplicant feh wget firefox imagemagick rofi scrot
+pacman -Sy --noconfirm lxdm xorg-xinit mesa xterm xorg-twm xorg-apps xorg-xclock i3-wm i3status i3lock dmenu perl-anyevent-i3 perl-json-xs synaptics net-tools light pulseaudio pavucontrol pulseaudio-alsa alsa-utils openssh vi ttf-font-awesome ttf-dejavu dialog wpa_supplicant feh wget firefox imagemagick rofi scrot iwd bash-completion powertop htop tlp nmon unzip compton ranger thunar atom tilda hexchat plank conky arc-gtk-theme zsh grml-zsh-config
 
 # Enable LXDM
 systemctl enable lxdm
