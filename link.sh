44f5ce1c55bee8b6e79c82c7f8fbfe93f77b75e4
Kadar Anwar <kadaranwar07@gmail.com>
2020-09-30 21:26:24 -0400

    Several changes
    Fixed a typo, commented out some lines and added sudo

--- link.sh
+++ link.sh
@@ -8,11 +8,13 @@
 
 # i3 config file first
 cp /home/kadar/dotfiles/i3/config /home/kadar/.config/i3/
+cp /home/kadar/dotfiles/i3/bar/top.conf /home/kadar/.config/i3/
+cp /home/kadar/dotfiles/i3/bar/bottom.conf /home/kadar/.config/i3/
 echo "i3 config copied!"
 sleep 2
 
 # pacman.conf
-cp /home/kadar/dotfiles/etc/pacman.conf /etc/
+sudo cp /home/kadar/dotfiles/etc/pacman.conf /etc/
 echo "pacman configuration file copied"
 sleep 2
 
@@ -21,24 +23,25 @@ cp /home/kadar/dotfiles/home/.bash_aliases /home/kadar/
 cp /home/kadar/dotfiles/home/.bashrc /home/kadar/
 cp /home/kadar/dotfiles/home/.Xresources /home/kadar/
 cp /home/kadar/dotfiles/home/.gitconfig /home/kadar/
+source ~/.bashrc
 echo "Files in home directory copied!"
 sleep 2
 
 # pictures for wallpaper
-»cp -r /home/kadar/dotfiles/Pictures/ /home/kadar/
+cp -r /home/kadar/dotfiles/Pictures/ /home/kadar/
 echo "Pictures/Wallpapers copied!"
 sleep 2
 
 # files in /boot
-cp /home/kadar/dotfiles/rEFInd/boot/intel-ucode.img /boot
-cp /home/kadar/dotfiles/rEFInd/boot/refind_linux.conf /boot
-echo "/boot files copied!"
-sleep 2
+#cp /home/kadar/dotfiles/rEFInd/boot/intel-ucode.img /boot
+#cp /home/kadar/dotfiles/rEFInd/boot/refind_linux.conf /boot
+#echo "/boot files copied!"
+#sleep 2
 
 # files for rEFInd
-cp /home/kadar/dotfiles/rEFInd/efi/refind.conf /efi/EFI/refind/
-echo "rEFInd files copied!"
-sleep 2
+#cp /home/kadar/dotfiles/rEFInd/efi/refind.conf /efi/EFI/refind/
+#echo "rEFInd files copied!"
+#sleep 2
 
 # bash powerline
 cp /home/kadar/dotfiles/scripts/bash-powerline/.bash-powerline.sh /home/kadar
