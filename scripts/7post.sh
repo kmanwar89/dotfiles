91d654d9278554d516a421d3cd979811ee5e5013
Kadar Anwar <kadaranwar07@gmail.com>
2020-09-30 21:51:57 -0400

    Fixed DNS
    Added systemd-resolved for DNS

--- scripts/7post.sh
+++ scripts/7post.sh
@@ -11,9 +11,13 @@ echo -e 'EnableNetworkConfiguration=true'
 echo -e '\n'
 echo -e '# randomizes mac-address every time iwd starts or the hardware is initially detected'
 echo -e 'AddressRandomization=once'
+echo -e '\n'
+echo -e '[Network]'
+echo -e 'NameResolvingService=systemd'
 
 # Enable/Start iwd service to connect to wireless
 #sudo systemctl enable --now iwd.service
 #sudo systemctl start iwd.service
 sudo systemctl enable --now iwd
+sudo systemctl enable --now systemd-resolved
 exit
